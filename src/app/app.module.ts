c0b3df3ac9fe629d664da1bc2d32f5806d37c71f
auretswanepoel <auret.swanepoel@practiceevolve.com.au>
2021-10-13 15:22:23 +1100

    4 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { InputHighlightModule } from '../input-highlight/input-highlight.module';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { MentionModule } from 'angular-mentions';
@@ -16,7 +15,7 @@ import { MentionModule } from 'angular-mentions';
     AngularMaterialModule,
     MentionModule,
   ],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
